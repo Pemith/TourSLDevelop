a04437b7bc7e072dd036bc63260c8addbee0bf34
Pemith <pemithr@gmail.com>
2021-08-30 11:49:10 +0530

    Merged And Finalized

--- Models/Restaurant.js
+++ Models/Restaurant.js
@@ -1,17 +1,34 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const restaurantSchema = new mongoose.Schema({
+const restaurantSchema= new mongoose.Schema({
 
+    cuisine:{
+        type: [String],
+        required: true,
+    },
+    menu: {
+        type: String
+    },
+    diningType:{
+        type:[String],
+        required: true
+    }
 
+});
 
-const restaurant = mongoose.model('Restaurants', restaurantSchema);
+const restaurant=mongoose.model('restaurant',restaurantSchema);
 
-function validateRestaurant(restaurant) {
+function validateRestaurant(restaurant){
 
-
+    const schema=Joi.object({
+        cuisine:Joi.string().required(),
+        menu: Joi.string().required(),
+        diningType:Joi.string().required()
+    }).options({abortEarly:false});
 
     return schema.validate(restaurant);
 }
 
-
+exports.Restaurant=restaurant;
+exports.validate=validateRestaurant;
--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
+const express = require('express');
+const app = express();
+const config=require('config');
 const activityProvider = require('./Routes/ActivityProviderRoute');
+const restaurant=require('./Routes/RestaurantRoute');
 const customer = require('./Routes/CustomerRoute');
+const authCustomer=require('./Routes/Auth');
+const admin=require('./Routes/AdminRoute');
+const authAdmin=require('./Routes/AuthAdmin');
+const site=require('./Routes/SiteRoute');
 const client = require('./Routes/ClientRoute');
 const clientLegalDocuments = require('./Routes/ClientLegalDocumentsRoute');
 const clientPhotos = require('./Routes/ClientPhotosRoute');
 const clientTemp = require('./Routes/ClientTempRoute');
-const express = require('express');
-const app = express();
-
-const config=require('config');
-const mongoose=require('mongoose');
-const Joi=require('joi');
-Joi.objectId=require('joi-objectid')(Joi);
-const express=require('express');
-const app=express();
-const site=require('./Routes/SiteRoute');
-const activityProvider=require('./Routes/ActivityProviderRoute');
-const restaurant=require('./Routes/RestaurantRoute');
-const customer=require('./Routes/CustomerRoute');
-const authCustomer=require('./Routes/Auth');
-const admin=require('./Routes/AdminRoute');
-const authAdmin=require('./Routes/AuthAdmin');
-
 
 if(!config.get('jwtPrivateKey')){
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
@@ -32,46 +22,29 @@ if(!config.get('jwtPrivateKey')){
 }
 
 
-const url = 'mongodb://localhost:27017/TourSLDB';
-
-mongoose.connect(url, {
+//const urlDB='mongodb+srv://pemithw:pemith12345@toursldb.rxady.mongodb.net/TourSLDB?retryWrites=true&w=majority';
+const urlDB='mongodb://localhost:27017/TourSLDB';
+mongoose.connect(urlDB, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true
     })
-    .then(() => console.log("Connected to MongoDB...", url))
+    .then(() => console.log("Connected to MongoDB...", urlDB))
     .catch(err => console.error('Could not connected to the DB', err));
 
 app.use(express.json());
 app.use('/api/activityprovider', activityProvider);
-app.use('/api/customer', customer);
-app.use('/api/client', client);
-app.use('/api/clientLegalDocuments', clientLegalDocuments);
-app.use('/api/clientPhotos', clientPhotos);
-app.use('/api/clientTemp', clientTemp);
-
-const urlDB='mongodb+srv://pemithw:pemith12345@toursldb.rxady.mongodb.net/TourSLDB?retryWrites=true&w=majority';
-//const urlDB='mongodb://localhost:27017/TourSLDB';
-
-mongoose.connect(urlDB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useFindAndModify:false,
-    useCreateIndex:true
-})
-    .then(()=>console.log("Connected to MongoDB...",urlDB))
-    .catch(err=>console.error('Could not connected to the DB',err));
-
-app.use(express.json());
-app.use('/api/activityprovider',activityProvider);
 app.use('/api/restaurant',restaurant);
-app.use('/api/customers',customer);
+app.use('/api/customer', customer);
 app.use('/api/auth',authCustomer);
 app.use('/api/admin',admin);
 app.use('/api/authadmin',authAdmin);
 app.use('/api/site',site);
-
+app.use('/api/client', client);
+app.use('/api/clientLegalDocuments', clientLegalDocuments);
+app.use('/api/clientPhotos', clientPhotos);
+app.use('/api/clientTemp', clientTemp);
 
 
 const port = process.env.PORT || 3000;
